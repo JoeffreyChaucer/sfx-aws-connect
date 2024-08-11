a87c8772026dd3c8602503901d7afafd60e783cf
joeffreychaucer <jeoffrey.paitan@qut.edu.au>
2024-08-11 14:24:46 +0800

    Add lambda type src/types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,17 +13,92 @@ export type TAwsAccessFlags = {
   
 export type TDownloadComponentParams = {
     connectClient?: ConnectClient;
-    LambdaClient?: LambdaClient;
-    instanceId: string;
+    lambdaClient?: LambdaClient;
+    instanceId?: string;
     componentType?: string;
-    outputDir: string;
+    outputDir?: string;
     id?: string;
     overrideFile?: boolean;
     download?: boolean;
   } & TAwsAccessFlags
   
   
-export type TComponentType = 'hoursOfOperation' | 'queues' | 'prompts' | 'flows' | 'routingProfiles' | 'lambda-functions';
+export type TComponentType = 'hoursOfOperation' | 'queues' | 'prompts' | 'flows' | 'routingProfiles' | 'lambdaFunctions';
 
+export type EnvironmentVariables = {
+  [key: string]: string | undefined;
+};
 
+export type Environment = {
+  Variables?: EnvironmentVariables;
+};
+
+export type TracingConfig = {
+  Mode?: string;
+};
+
+export type Layer = {
+  Arn?: string;
+  CodeSize?: number;
+};
+
+export type EphemeralStorage = {
+  Size?: number;
+};
+
+export type SnapStart = {
+  ApplyOn?: string;
+  OptimizationStatus?: string;
+};
+
+export type RuntimeVersionConfig = {
+  RuntimeVersionArn?: string;
+};
+
+export type LoggingConfig = {
+  LogFormat?: string;
+  LogGroup?: string;
+};
+
+export type LambdaFunctionConfiguration = {
+  FunctionName?: string;
+  FunctionArn?: string;
+  Runtime?: string;
+  Role?: string;
+  Handler?: string;
+  CodeSize?: number;
+  Description?: string;
+  Timeout?: number;
+  MemorySize?: number;
+  LastModified?: string;
+  CodeSha256?: string;
+  Version?: string;
+  Environment?: Environment;
+  TracingConfig?: TracingConfig;
+  RevisionId?: string;
+  Layers?: Layer[];
+  State?: string;
+  LastUpdateStatus?: string;
+  PackageType?: string;
+  Architectures?: string[];
+  EphemeralStorage?: EphemeralStorage;
+  SnapStart?: SnapStart;
+  RuntimeVersionConfig?: RuntimeVersionConfig;
+  LoggingConfig?: LoggingConfig;
+};
+
+export type LambdaFunctionCode = {
+  RepositoryType?: string;
+  Location?: string;
+};
+
+export type LambdaFunctionTags = {
+  [key: string]: string | undefined;
+};
+
+export type TLambdaFunction = {
+  Configuration?: LambdaFunctionConfiguration;
+  Code?: LambdaFunctionCode;
+  Tags?: LambdaFunctionTags;
+};
 
