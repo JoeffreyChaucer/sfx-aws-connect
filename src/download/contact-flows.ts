19c4475735a6512660dea128628ed17dcca501c3
joeffreychaucer <jeoffrey.paitan@qut.edu.au>
2024-08-15 13:09:28 +0800

    Refactor contact flow

--- src/download/contact-flows.ts
+++ src/download/contact-flows.ts
@@ -1,116 +1,95 @@
-import { AccessDeniedException, DescribeContactFlowCommand, DescribeContactFlowCommandOutput, ContactFlow as IContactFlow, ListContactFlowsCommand, ListContactFlowsCommandOutput, ResourceNotFoundException, } from "@aws-sdk/client-connect";
-import path from "node:path";
+import type { ConnectClient, DescribeContactFlowCommandOutput, ListContactFlowsCommandOutput } from "@aws-sdk/client-connect";
 
-import { AwsComponentData, AwsComponentFileWriter } from '../services/aws-component-file-writer.js';
-import { TDownloadComponentParams } from '../types/index.js';
+import {
+  DescribeContactFlowCommand,
+  ContactFlow as IContactFlow,
+  ListContactFlowsCommand,
+  ResourceNotFoundException
+} from "@aws-sdk/client-connect";
 
-interface ContactFlow extends AwsComponentData {
+export interface ContactFlow {
   ContactFlow: IContactFlow;
 }
 
-export async function downloadSpecificContactFlow({
-  connectClient,
-  instanceId,
-  componentType,
-  id: contactFlowId,
-  outputDir,
-  overWrite
-}: TDownloadComponentParams): Promise<string | null> {
-  if (!connectClient) {
-    throw new Error('ConnectClient is not provided');
-  }
+export async function downloadSpecificContactFlow(
+  connectClient: ConnectClient,
+  instanceId: string,
+  contactFlowId?: string,
+  name?: string
+): Promise<ContactFlow | undefined> {
+  let contactFlowIdToUse = contactFlowId;
   
-  let safeOutputDir: string;
-  
-  const writer = new AwsComponentFileWriter<ContactFlow>();
-
-  if (componentType === 'all') {
-    safeOutputDir = path.join(outputDir || process.cwd(), 'contactFlows');
-  } else {
-    const defaultOutputDir = path.join(process.cwd(), 'metadata', 'contactFlows');
-    safeOutputDir = outputDir || defaultOutputDir;
-  }
-
-  
-  try {
-    const describeResponse: DescribeContactFlowCommandOutput = await connectClient.send(new DescribeContactFlowCommand({
-      InstanceId: instanceId,
-      ContactFlowId: contactFlowId
-    }));
-
-    const contactFlow: IContactFlow | undefined = describeResponse.ContactFlow;
-    if (!contactFlow) return null;
-    
-    const restructuredData: ContactFlow = {
-      ContactFlow: {
-        Name: contactFlow.Name,
-        Arn: contactFlow.Arn,
-        Id: contactFlow.Id,
-        Type: contactFlow.Type,
-        State: contactFlow.State,
-        Status: contactFlow.Status,
-        Description: contactFlow.Description,
-        Content: contactFlow.Content,
-        Tags: contactFlow.Tags,
-      }
-    };
-    
-    const fileName: string | undefined = contactFlow.Name;
-    await writer.writeComponentFile(safeOutputDir, restructuredData, overWrite);
-    
-    return fileName ?? null;
-  } catch (error: any) {
-    if (error instanceof ResourceNotFoundException) {
-      console.warn(`Contact Flow with ID ${contactFlowId} not found. It may have been deleted.`);
-    } else if (error instanceof Error && error.message.includes('Invalid parameter')) {
-      console.warn(`Invalid Contact flow ID: ${contactFlowId}. Please check the ID and ensure it's correctly formatted.`);
-    } else if (error instanceof AccessDeniedException) {
-      console.error(`Access denied: You don't have permission to access flow ${contactFlowId}.`);
-    } else {
-      console.error(`Unexpected error downloading contact flow ${contactFlowId}:`, error.message);
-
+  if (name && !contactFlowId) {
+    contactFlowIdToUse = await getIdByName(connectClient, instanceId, name);
+    if (!contactFlowIdToUse) {
+      throw new ResourceNotFoundException({
+        message: `Contact Flow with name "${name}" not found.`,
+        $metadata: {}
+      });
     }
-
-    return null;
   }
+  
+  const command: DescribeContactFlowCommand = new DescribeContactFlowCommand({
+    InstanceId: instanceId,
+    ContactFlowId: contactFlowIdToUse
+  });
+ 
+  const response: DescribeContactFlowCommandOutput = await connectClient.send(command);
+  
+  if(!response.ContactFlow) return undefined
+  
+  const contactFlow: IContactFlow = response.ContactFlow
+  
+  const contactFlowData: ContactFlow = {
+    ContactFlow: {
+      Arn: contactFlow.Arn,
+      Id: contactFlow.Id,
+      Name: contactFlow.Name,
+      Type: contactFlow.Type,
+      State: contactFlow.State,
+      Status: contactFlow.Status,
+      Description: contactFlow.Description,
+      Content: contactFlow.Content,
+      Tags: contactFlow.Tags,
+    }
+  };
+
+  return contactFlowData;
 }
 
-export async function downloadAllContactFlows({
-  connectClient,
-  instanceId,
-  componentType,
-  outputDir,
-  overWrite
-}: TDownloadComponentParams): Promise<string[]> {
-  
-  if (!connectClient) {
-    throw new Error('ConnectClient is not provided');
-  }
+async function getIdByName(connectClient: ConnectClient, instanceId: string, name: string): Promise<string | undefined> {
+    const command = new ListContactFlowsCommand({ InstanceId: instanceId });
+    const response: ListContactFlowsCommandOutput = await connectClient.send(command);
 
-  const listResponse: ListContactFlowsCommandOutput = await connectClient.send(new ListContactFlowsCommand({ InstanceId: instanceId }));
+    const contactFlowId = response.ContactFlowSummaryList?.find(flow => flow.Name === name);
+
+    return contactFlowId?.Id;
+}
+
+export async function downloadAllContactFlows( 
+  connectClient: ConnectClient,
+  instanceId: string,
+): Promise<(ContactFlow | undefined)[]> {
+  
+  const command: ListContactFlowsCommand = new ListContactFlowsCommand({ InstanceId: instanceId })
+  const listResponse: ListContactFlowsCommandOutput = await connectClient.send(command);
 
   if (!listResponse.ContactFlowSummaryList || listResponse.ContactFlowSummaryList.length === 0) {
     console.warn('No Contact Flows found for this Connect instance.');
     return [];
   }
 
-  const downloadPromises: Promise<string | null>[] = listResponse.ContactFlowSummaryList
-    .filter(summary => 
-      summary.Id && 
-      summary.Name)
-    .map(summary => 
-      downloadSpecificContactFlow({
-        connectClient,
-        instanceId,
-        componentType,
-        outputDir,
-        overWrite,
-        id: summary.Id!,
-      }).catch(() => null)
-    );
-      
-  const downloadResults: (string | null)[] = await Promise.all(downloadPromises);
+  const contactFlows: Promise<ContactFlow | undefined>[] = listResponse.ContactFlowSummaryList
+  .filter((ContactFlowSummary) => ContactFlowSummary.Id && ContactFlowSummary.Name)  // Filter out items without an ID and Names
+  .map((ContactFlowSummary) =>
+    downloadSpecificContactFlow(
+      connectClient,
+      instanceId,
+      ContactFlowSummary.Id!
+    )
+  );
+  
+  const contactFlowData: (ContactFlow | undefined)[] = await Promise.all(contactFlows)
 
-  // Filter out null results (failed downloads) and return successful downloads
-  return downloadResults.filter((result): result is string => result !== null);
+  return contactFlowData
 }
